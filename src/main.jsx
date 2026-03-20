6f42b7d9b6ed1e3bb466eaf7d120755b096cd8fc
Marquee <lorenzbarangan112@gmail.com>
2026-03-20 21:38:42 +0800

    fix: move registerSW import to top of main.jsx per ES module convention

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.jsx";
 import "./index.css";
 import { queryClientConfig } from "./api.js";
+import { registerSW } from "virtual:pwa-register";
 
 const queryClient = new QueryClient(queryClientConfig);
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -27,7 +28,6 @@ ReactDOM.createRoot(rootEl).render(
 
 inject();
 
-import { registerSW } from "virtual:pwa-register";
 
 registerSW({
   onUpdatedSW() {
